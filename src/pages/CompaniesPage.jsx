17047f4c5f232afb3f744a02f19aa16176af7be1
Rox <you@example.com>
2021-12-08 14:39:24 +0100

    companiesPage to edit

--- src/pages/CompaniesPage.jsx
+++ src/pages/CompaniesPage.jsx
@@ -2,15 +2,34 @@ import "../App.css";
 import Searchbar from "../components/Searchbar/Searchbar";
 import CardCompanies from "../components/CardCompanies/CardCompanies"
 
+import axios from 'axios'
+import { useState, useEffect} from 'react';
+import { Row } from "antd";
 
+const getCompaniesService = axios.create({
+    baseURL: `${process.env.REACT_APP_SERVER_URL}/companies`,
+  });
 
 const CompaniesPage = ()=>{
+    const [companiesList, setCompaniesList] = useState([])
 
+    useEffect(()=> {
+    axios.get(getCompaniesService).then((response) => {
+        setCompaniesList(response.data.searchCompany)
+    });
+}, [])
     return (
         <div className="CompaniesPage">
             <h1>Companies</h1>
             <Searchbar />
-            <CardCompanies />
+
+            <Row style={{ width: "100%", justifyContent: "center" }}> 
+            {companiesList.map(companies => {
+            return(
+              <CardCompanies companies={companies} /> 
+            )
+            })}
+            </Row>  
         </div>
     )
 }
