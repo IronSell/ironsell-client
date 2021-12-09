e33a6e5c928e396a0e03e6e4fb52638def7062b7
Rox <you@example.com>
2021-12-09 19:09:30 +0100

    changes routes

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-import { Navigate } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import Login from "../pages/LogIn";
-import Signup from "../pages/Signup";
-import UserPage from "../pages/UserPage";
-import * as PATHS from "../utils/paths";
-import OffersJobPage from "../pages/OffersJobPage";
-import CompaniesPage from "../pages/CompaniesPage";
-=======
 import { Navigate } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import Login from '../pages/LogIn';
 import Signup from '../pages/Signup';
-import ProtectedPage from '../pages/ProtectedPage';
+import UserPage from '../pages/UserPage';
 import * as PATHS from '../utils/paths';
 import OffersJobPage from '../pages/OffersJobPage';
 import CompaniesPage from '../pages/CompaniesPage';
 import CompanyPage from '../pages/CompanyPage';
 
->>>>>>> 3b008cf92626149a77f999e49ba7fdd2942043d0
 const routes = (props) => {
   const { user } = props;
   return [
