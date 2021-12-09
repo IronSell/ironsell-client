c2bbe0e2a28b30b2a63cbb35f138cc1d72373f27
nachbr91 <nabero91@gmail.com>
2021-12-09 15:49:06 +0100

    Fix get user details route on profile page

--- src/pages/ProtectedPage.jsx
+++ src/pages/ProtectedPage.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
-import * as USER_HELPERS from "../utils/userToken";
+import { useEffect } from 'react';
+import * as USER_HELPERS from '../utils/userToken';
 
-const ProtectedPage = () => {
-  const [user, setUser] = useState([]);
- 
+const ProtectedPage = (props) => {
+  const { authenticate, user } = props;
   useEffect(() => {
     axios
-      .get('http://localhost:5005/api/users/profile/session', {
+      .get(`http://localhost:5005/api/users/profile/${user._id}`, {
         headers: {
-          Authorization: USER_HELPERS.getUserToken()
+          Authorization: USER_HELPERS.getUserToken(),
         },
       })
       .then((response) => {
-        console.log(response.session);
-        setUser(response.data.showUser);
+        console.log(response.data);
+        authenticate(response.data.showUser);
       });
   }, []);
 
   return (
     <div>
       <h1>User Page</h1>
+      <p>
+        {user.name} {user.lastName}
+      </p>
     </div>
   );
 };
--- src/services/offers.js
+++ src/services/offers.js
@@ -1,37 +1,37 @@
-// import axios from 'axios';
+import axios from 'axios';
+import * as USER_HELPERS from "../utils/userToken";
 
-// function internalServerError(err) {
-//   if (err.response && err.response.data && err.response.data.errorMessage) {
-//     return {
-//       status: false,
-//       errorMessage: err.response.data.errorMessage,
-//     };
-//   }
-//   return {
-//     status: false,
-//     errorMessage: 'Internal server error. Please check your server',
-//   };
-// }
+function internalServerError(err) {
+  if (err.response && err.response.data && err.response.data.errorMessage) {
+    return {
+      status: false,
+      errorMessage: err.response.data.errorMessage,
+    };
+  }
+  return {
+    status: false,
+    errorMessage: 'Internal server error. Please check your server',
+  };
+}
 
-// function successStatus(res) {
-//   return {
-//     status: true,
-//     data: res.data,
-//   };
-// }
+function successStatus(res) {
+  return {
+    status: true,
+    data: res.data,
+  };
+}
 
-// // creates a basic url for every request in this file
-// const getOffersService = axios.create({
-//   baseURL: `${process.env.REACT_APP_SERVER_URL}/offers`,
-// });
+// creates a basic url for every request in this file
+const getOffersService = axios.create({
+  baseURL: `${process.env.REACT_APP_SERVER_URL}/offers`,
+  headers: {
+    Authorization: USER_HELPERS.getUserToken(),
+  },
+});
 
-// export function getOffers(jobOffers) {
-//   return getOffersService
-//     .get(`jobOffers`, {
-//       headers: {
-
-//       },
-//     })
-//     .then(successStatus)
-//     .catch(internalServerError);
-// }
+export function getOffers() {
+  return getOffersService
+    .get(`jobOffers`)
+    .then(successStatus)
+    .catch(internalServerError);
+}
