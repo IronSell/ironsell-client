8aa130a30b846977b9f11f79d29f9d21e332e019
Rox <you@example.com>
2021-12-10 17:15:25 +0100

    pffffffffff

---
+++ src/pages/OfferPage.jsx
@@ -0,0 +1,29 @@
+import '../App.css';
+import { useState, useEffect } from 'react';
+import { getCompany } from '../services/companies';
+
+const CompanyPage = () => {
+  const [company, setCompany] = useState([]);
+
+  let urlStr = window.location.href;
+  let url = new URL(urlStr);
+  let search_params = url.searchParams;
+  let id = search_params.get('id');
+
+  useEffect(() => {
+    getCompany(id).then((response) => {
+      // console.log({ response });
+      setCompany(response.data.showCompany);
+    });
+  },[]);
+
+  return (
+    <div className='CompanyPage'>
+      <h1>{company.name}</h1>
+      <h2>{company.companyDescription}</h2>
+      <div>{/* <CardCompanyByUser showCompany={showCompany} /> */}</div>
+    </div>
+  );
+};
+
+export default CompanyPage;
