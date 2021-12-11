76527349058bab0e7d1ad3ce399524f88200a58f
nachbr91 <nabero91@gmail.com>
2021-12-11 19:10:44 +0100

    Edit offer card styles

--- src/components/CardOffers/CardOffer.jsx
+++ src/components/CardOffers/CardOffer.jsx
@@ -1,31 +1,31 @@
-import { List, Avatar } from 'antd'
+import { List, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import * as PATHS from '../../utils/paths';
-import './CardOffers.css'
+import './CardOffers.css';
 
 function CardOffer(props) {
-  const { searchOffers } = props
-  const data = [
-    searchOffers];
+  const { searchOffers } = props;
+  const data = [searchOffers];
   return (
-    <div className="App">
+    <div className='App'>
       <Link to={PATHS.OFFERPAGE + '?id=' + searchOffers._id}>
-      <List
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={(item) => (
-          <List.Item className='cardOfferBorder'>
-            <List.Item.Meta className='cardOfferBorderListItem'
-              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-              title={<a href="https://ant.design">{searchOffers.jobTitle}</a>}
-              description={searchOffers.description}
-            />
-          </List.Item>
-        )}
+        <List
+          itemLayout='horizontal'
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item className='cardOfferBorder'>
+              <List.Item.Meta
+                className='cardOfferBorderListItem'
+                avatar={<Avatar src='https://joeschmoe.io/api/v1/random' />}
+                title={searchOffers.jobTitle}
+                description={searchOffers.description}
+              />
+            </List.Item>
+          )}
         />
       </Link>
     </div>
-  )
+  );
 }
 
-export default CardOffer
+export default CardOffer;
