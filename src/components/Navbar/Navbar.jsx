d7b34f72169bb7f9ea47ca9df79ef16c9aa10771
nachbr91 <nabero91@gmail.com>
2021-12-11 12:51:28 +0100

    Delete console.log(user) from navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,44 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
-import * as PATHS from '../../utils/paths'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import * as PATHS from '../../utils/paths';
 // import * as CONSTS from "../../utils/consts";
 
 const Navbar = (props) => {
-  const { user, handleLogout} = props
+  const { user, handleLogout } = props;
   return (
     <nav>
-      <Link to={PATHS.HOMEPAGE} className="nav__projectName">
+      <Link to={PATHS.HOMEPAGE} className='nav__projectName'>
         IronJob
       </Link>
       <Link to={PATHS.OFFERSJOBPAGE}>Offers</Link>
       <Link to={PATHS.COMPANIESPAGE}>Companies</Link>
 
-      <div className="nav__authLinks">
-      {console.log(user)}
+      <div className='nav__authLinks'>
         {user ? (
-          
           <>
-            <Link to={PATHS.USERPROFILE} className="authLink">
+            <Link to={PATHS.USERPROFILE} className='authLink'>
               {user.name} {user.lastName}
             </Link>
-            <button className="nav-logoutbtn" onClick={handleLogout}>
+            <button className='nav-logoutbtn' onClick={handleLogout}>
               Logout
             </button>
           </>
         ) : (
           <>
-            <Link to={PATHS.SIGNUPPAGE} className="authLink">
+            <Link to={PATHS.SIGNUPPAGE} className='authLink'>
               Signup
             </Link>
-            <Link to={PATHS.LOGINPAGE} className="authLink">
+            <Link to={PATHS.LOGINPAGE} className='authLink'>
               Log In
             </Link>
           </>
         )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
