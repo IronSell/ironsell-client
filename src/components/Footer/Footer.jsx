c4d57e131c5722dadfca0940b896dd2effafb1e3
Federico Altava Redondo <fedealtava@gmail.com>
2021-12-14 19:14:33 +0100

    Footer fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {GithubOutlined, LinkedinOutlined } from '@ant-design/icons'
 import '../Footer/Footer.css';
 
 function Footer() {
@@ -9,29 +9,21 @@ function Footer() {
 
       <div className='columsFooter'>
         <p>Rocio Garcia:</p>
-        
-        <Link className="btn btn-primary" style="background-color: #0082ca;" href="#!" role="button">
-            <i className="fab fa-linkedin-in"> </i></Link>
-            <Link className="btn btn-primary" style="background-color: #333333;" href="#!" role="button">
-                <i className="fab fa-github"></i></Link>
+        <GithubOutlined className='logoGit'/>
+        <LinkedinOutlined style={{color:'#1457b6'}}/>
       </div>
 
-        <div className='columsFooter'>
-            <p>Nacho Benito:</p>
-            <Link className="btn btn-primary" style="background-color: #0082ca;" href="#!" role="button">
-            <i className="fab fa-linkedin-in"> cc</i></Link>
-            <Link className="btn btn-primary" style="background-color: #333333;" href="#!" role="button">
-                <i className="fab fa-github"></i></Link>
-        </div>
+      <div className='columsFooter'>
+        <p>Nacho Benito:</p>
+        <GithubOutlined className='logoGit'/>
+        <LinkedinOutlined style={{color:'#1457b6'}}/>
+      </div>
 
-            <div className='columsFooter'>
-            <p>federico Altava Redondo:</p>
-            <Link className="btn btn-primary" style="background-color: #0082ca;" href="#!" role="button">
-            <i className="fab fa-linkedin-in"> </i></Link>
-            <Link className="btn btn-primary" style="background-color: #333333;" href="#!" role="button">
-                <i className="fab fa-github"></i></Link>
-                
-            </div>
+      <div className='columsFooter'>
+        <p>federico Altava Redondo:</p>
+        <GithubOutlined className='logoGit'/>
+        <LinkedinOutlined style={{color:'#1457b6'}}/>
+      </div>
     </div>
   );
 }
--- src/pages/OfferPage.jsx
+++ src/pages/OfferPage.jsx
@@ -23,7 +23,7 @@ const OfferPage = () => {
       <div className="card col-md-12 p-3">
         <div className="row ">
           <div className="col-md-4">
-            <img className="w-100" src="https://superhappygames.com/wp-content/uploads/2018/08/Word-Explorer-Icon-350x350px-300x300.png" />
+            <img className="w-100" src="https://www.top-employers.com/contentassets/11f5c0206ef34308af4c76f429cf6f19/oid00d200000000wi7ids0683y000008hunyda3y0000005tfbha68odegbbbyzywsyis6wtee5ks_ozkx8vqvgrqknt4aspdffalse2" />
           </div>
           <div className="col-md-8">
             <div className="card-block">
@@ -34,7 +34,7 @@ const OfferPage = () => {
               <p className="card-text text-justify">{offer.requirements}</p>
               <p className="card-text text-justify">{offer.schedule}</p>
               <p className="card-text text-justify">{offer.description}</p>
-              <a href="https://www.google.com" className="btn btn-primary">read more...</a>
+              <a href="https://www.google.com" className="btn btn-primary">apply</a>
             </div>
           </div>
         </div>
