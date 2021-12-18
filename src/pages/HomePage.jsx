0cc74a245fc5c77a9712ec36666a17071f340187
nachbr91 <nabero91@gmail.com>
2021-12-18 01:47:50 +0100

    Edit app styles, fix padding top for all views. Edit companies card component, fix company logo width and height

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,10 +3,12 @@ import Searchbar from '../components/Searchbar/Searchbar';
 
 function HomePage() {
   return (
-    <div className='App'>
-      <h1>Home Page</h1>
-      <Searchbar />
-    </div>
+    <main className='App'>
+      <div className='container'>
+        <h1>Home Page</h1>
+        <Searchbar />
+      </div>
+    </main>
   );
 }
 
