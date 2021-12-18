49667d029fbb2edea106d61044571c9a51ef32b6
nachbr91 <nabero91@gmail.com>
2021-12-18 14:01:08 +0100

    Edit company profile view

--- src/pages/OfferPage.jsx
+++ src/pages/OfferPage.jsx
@@ -17,25 +17,6 @@ const OfferPage = () => {
   }, []);
 
   return (
-<<<<<<< HEAD
-    <div className="container">
-    <div className="row">
-      <div className="card col-md-12 p-3">
-        <div className="row ">
-          <div className="col-md-4">
-            <img className="w-100" src="https://www.top-employers.com/contentassets/11f5c0206ef34308af4c76f429cf6f19/oid00d200000000wi7ids0683y000008hunyda3y0000005tfbha68odegbbbyzywsyis6wtee5ks_ozkx8vqvgrqknt4aspdffalse2" />
-          </div>
-          <div className="col-md-8">
-            <div className="card-block">
-              <h1 className="card-title">{offer.jobTitle}</h1>
-              <p className="card-text text-justify">{offer.province}</p>
-              <p className="card-text text-justify">{offer.schedule}</p>
-              <p className="card-text text-justify">{offer.salary}</p>
-              <p className="card-text text-justify">{offer.experienceYears}</p>
-              <p className="card-text text-justify">{offer.requeriments}</p>
-              <p className="card-text text-justify">{offer.description}</p>
-              <a href="https://www.google.com" className="btn btn-primary">apply</a>
-=======
     <div className='container'>
       <div className='row'>
         <div className='card col-md-12 p-3'>
@@ -62,7 +43,6 @@ const OfferPage = () => {
                   apply
                 </a>
               </div>
->>>>>>> 9e4f792b4ad3fd76d2834bf52da547b66529b70b
             </div>
           </div>
         </div>
