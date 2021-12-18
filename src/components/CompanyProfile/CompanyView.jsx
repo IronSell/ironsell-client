9e4f792b4ad3fd76d2834bf52da547b66529b70b
nachbr91 <nabero91@gmail.com>
2021-12-18 03:33:34 +0100

    Edit company profile styles

--- src/components/CompanyProfile/CompanyView.jsx
+++ src/components/CompanyProfile/CompanyView.jsx
@@ -1,5 +1,18 @@
 import './CompanyProfile.css';
-import { Image, Typography, Anchor, Text, Descriptions, Collapse } from 'antd';
+import {
+  Image,
+  Typography,
+  Anchor,
+  Text,
+  Descriptions,
+  Collapse,
+  Divider,
+} from 'antd';
+import {
+  LinkedinOutlined,
+  FacebookOutlined,
+  InstagramOutlined,
+} from '@ant-design/icons';
 
 function CompanyView(props) {
   const {
@@ -25,21 +38,26 @@ function CompanyView(props) {
       <div className='CompanyView'>
         <div className='company-logo-container'>
           <img className='company-logo' src={companyLogo} alt={name} />
-          <h3>{name}</h3>
+          <Title level={3}>{name}</Title>
           <div className='logo-container'></div>
         </div>
         <div className='info-container'>
           <Descriptions
             title='Company info'
             bordered
-            column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
+            column={{ lg: 3, md: 3, sm: 2, xs: 1 }}
           >
-            <Descriptions.Item label='Email'>{email}</Descriptions.Item>
-            <Descriptions.Item label='CIF'>{cif}</Descriptions.Item>
             <Descriptions.Item label='Professional Sector'>
               {professionalSector}
             </Descriptions.Item>
-            <Descriptions.Item label='Provice'>{province}</Descriptions.Item>
+            <Descriptions.Item label='CIF'>{cif}</Descriptions.Item>
+            <Descriptions.Item label='Website' span={2}>
+              {companyUrl}
+            </Descriptions.Item>
+            <Descriptions.Item label='Email' span={2}>
+              {email}
+            </Descriptions.Item>
+            <Descriptions.Item label='Province'>{province}</Descriptions.Item>
             <Descriptions.Item label='Address'>{address}</Descriptions.Item>
           </Descriptions>
           <Collapse className='company-description' defaultActiveKey={['1']}>
@@ -49,10 +67,22 @@ function CompanyView(props) {
           </Collapse>
         </div>
         <div className='media-container'>
-          <Title level={2}>Published job offers:</Title>
+          <Title level={3}>Social media</Title>
+          <Divider />
+          <Text>
+            <LinkedinOutlined /> /quimiromar
+          </Text>
+          <Divider />
+          <Text>
+            <FacebookOutlined />quimiromar
+          </Text>
+          <Divider />
+          <Text>
+            <InstagramOutlined /> @quimiromar
+          </Text>
         </div>
         <div className='offers-container'>
-          <Title level={2}>Published job offers:</Title>
+          <Title level={3}>Active job vacancies</Title>
         </div>
       </div>
     </main>
