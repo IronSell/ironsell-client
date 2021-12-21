d93967ccb04d1c0e33f2c52456b4d2cc4f72c6e8
Federico Altava Redondo <fedealtava@gmail.com>
2021-12-21 11:44:14 +0100

    offerjob

--- src/pages/OfferPage.jsx
+++ src/pages/OfferPage.jsx
@@ -26,11 +26,11 @@ const OfferPage = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="card col-md-12 p-3">
+        <div className="card col-md-12 p-3 border-color">
           <div className="row ">
             <div className="col-md-4">
               <img
-                className="w-100"
+                className="w-60"
                 src="https://www.top-employers.com/contentassets/11f5c0206ef34308af4c76f429cf6f19/oid00d200000000wi7ids0683y000008hunyda3y0000005tfbha68odegbbbyzywsyis6wtee5ks_ozkx8vqvgrqknt4aspdffalse2"
                 alt="Company logo"
               />
@@ -38,15 +38,16 @@ const OfferPage = () => {
             <div className="col-md-8">
               <div className="card-block">
                 <h1 className="card-title">{offer.jobTitle}</h1>
-                <p className="card-text text-justify">{offer.salary}</p>
-                <p className="card-text text-justify">
+                <hr/>
+                <p className="card-text text-justify color-description-offer">{offer.salary}</p>
+                <p className="card-text text-justify color-description-offer">
                   {offer.experienceYears}
                 </p>
-                <p className="card-text text-justify">{offer.province}</p>
+                <p className="card-text text-justify color-description-offer">{offer.province}</p>
                 <p className="card-text text-justify">{offer.requirements}</p>
-                <p className="card-text text-justify">{offer.schedule}</p>
+                <p className="card-text text-justify color-description-offer">{offer.schedule}</p>
                 <p className="card-text text-justify">{offer.description}</p>
-                <button onClick={addToFav}></button>
+                <button className="ApllyButton" onClick={addToFav}>Apply</button>
               </div>
             </div>
           </div>
